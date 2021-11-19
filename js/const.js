110c439b0ba5b5037ee0750fee142ac0491075da
null-game <85382354+null-game@users.noreply.github.com>
2021-11-19 12:57:15 +0900

    パターン定義修正

--- js/const.js
+++ js/const.js
@@ -18,48 +18,50 @@ const pattern = [
 const vectorPattern = [
 	[
 		// Iミノ以外
-		[
+		{
 			// angle: 0
-			[Vector2.ZERO, Vector2.RIGHT, Vector2.RIGHT_UP, Vector2.DOWN.times(2), Vector2.RIGHT.add(Vector2.DOWN.times(2))],
-			[Vector2.ZERO, Vector2.LEFT, Vector2.ONE.inverse, Vector2.DOWN.times(2), Vector2.LEFT.add(Vector2.DOWN.times(2))],
-		],
-		[
+			LEFT: [Vector2.ZERO, Vector2.RIGHT, Vector2.RIGHT_UP, Vector2.DOWN.times(2), Vector2.add(Vector2.RIGHT, Vector2.DOWN.times(2))],
+			RIGHT: [Vector2.ZERO, Vector2.LEFT, Vector2.ONE.inverse, Vector2.DOWN.times(2), Vector2.add(Vector2.LEFT, Vector2.DOWN.times(2))],
+		},
+		{
 			// angle: 90
-			[Vector2.ZERO, Vector2.RIGHT, Vector2.ONE, Vector2.UP.times(2), Vector2.RIGHT.add(Vector2.UP.times(2))],
-			[Vector2.ZERO, Vector2.RIGHT, Vector2.ONE, Vector2.UP.times(2), Vector2.RIGHT.add(Vector2.UP.times(2))],
-		],
-		[
+			LEFT: [Vector2.ZERO, Vector2.RIGHT, Vector2.ONE, Vector2.UP.times(2), Vector2.add(Vector2.RIGHT, Vector2.UP.times(2))],
+			RIGHT: [Vector2.ZERO, Vector2.RIGHT, Vector2.ONE, Vector2.UP.times(2), Vector2.add(Vector2.RIGHT, Vector2.UP.times(2))],
+		},
+		{
 			// angle: 180
-			[Vector2.ZERO, Vector2.LEFT, Vector2.ONE.inverse, Vector2.DOWN.times(2), Vector2.LEFT.add(Vector2.DOWN.times(2))],
-			[Vector2.ZERO, Vector2.RIGHT, Vector2.RIGHT_UP, Vector2.DOWN.times(2), Vector2.RIGHT.add(Vector2.DOWN.times(2))],
-		],
-		[
+			LEFT: [Vector2.ZERO, Vector2.LEFT, Vector2.ONE.inverse, Vector2.DOWN.times(2), Vector2.add(Vector2.LEFT, Vector2.DOWN.times(2))],
+			RIGHT: [Vector2.ZERO, Vector2.RIGHT, Vector2.RIGHT_UP, Vector2.DOWN.times(2), Vector2.add(Vector2.RIGHT, Vector2.DOWN.times(2))],
+		},
+		{
 			// angle: 270
-			[Vector2.ZERO, Vector2.LEFT, Vector2.LEFT_DOWN, Vector2.UP.times(2), Vector2.LEFT.add(Vector2.UP.times(2))],
-			[Vector2.ZERO, Vector2.LEFT, Vector2.LEFT_DOWN, Vector2.UP.times(2), Vector2.LEFT.add(Vector2.UP.times(2))],
-		],
+			LEFT: [Vector2.ZERO, Vector2.LEFT, Vector2.LEFT_DOWN, Vector2.UP.times(2), Vector2.add(Vector2.LEFT, Vector2.UP.times(2))],
+			RIGHT: [Vector2.ZERO, Vector2.LEFT, Vector2.LEFT_DOWN, Vector2.UP.times(2), Vector2.add(Vector2.LEFT, Vector2.UP.times(2))],
+		},
 	],
 	[
 		// Iミノ
-		[
+		{
 			// angle: 0
-			[Vector2.ZERO, Vector2.LEFT, Vector2.RIGHT.times(2), Vector2.LEFT.add(Vector2.UP.times(2)), new Vector2(2, 1)],
-			[Vector2.ZERO, Vector2.LEFT.times(2), Vector2.RIGHT, new Vector2(-2, 1), Vector2.RIGHT.add(Vector2.UP.times(2))],
-		],
-		[
+			LEFT: [Vector2.ZERO, Vector2.LEFT, Vector2.RIGHT.times(2), Vector2.add(Vector2.LEFT, Vector2.UP.times(2)), Vector2.add(Vector2.RIGHT.times(2), Vector2.DOWN)],
+			RIGHT: [Vector2.ZERO, Vector2.LEFT.times(2), Vector2.RIGHT, Vector2.add(Vector2.LEFT.times(2), Vector2.DOWN), Vector2.add(Vector2.RIGHT, Vector2.UP.times(2))],
+		},
+		{
 			// angle: 90
-			[Vector2.ZERO, Vector2.RIGHT.times(2), Vector2.LEFT, new Vector2(2, -1), Vector2.LEFT.add(Vector2.DOWN.times(2))],
-			[Vector2.ZERO, Vector2.LEFT, Vector2.RIGHT.times(2), Vector2.LEFT.add(Vector2.UP.times(2)), new Vector2(2, 1)],
-		],
-		[
+			LEFT: [Vector2.ZERO, Vector2.RIGHT.times(2), Vector2.LEFT, Vector2.add(Vector2.RIGHT.times(2), Vector2.UP), Vector2.add(Vector2.LEFT, Vector2.DOWN.times(2))],
+			RIGHT: [Vector2.ZERO, Vector2.LEFT, Vector2.RIGHT.times(2), Vector2.add(Vector2.LEFT, Vector2.UP.times(2)), Vector2.add(Vector2.RIGHT.times(2), Vector2.DOWN)],
+		},
+		{
 			// angle: 180
-			[Vector2.ZERO, Vector2.RIGHT, Vector2.LEFT.times(2), Vector2.RIGHT.add(Vector2.DOWN.times(2)), new Vector2(-2, -1)],
-			[Vector2.ZERO, Vector2.RIGHT.times(2), Vector2.LEFT, new Vector2(2, -1), Vector2.LEFT.add(Vector2.DOWN.times(2))],
-		],
-		[
+			LEFT: [Vector2.ZERO, Vector2.RIGHT, Vector2.LEFT.times(2), Vector2.add(Vector2.RIGHT, Vector2.DOWN.times(2)), Vector2.add(Vector2.LEFT.times(2), Vector2.UP)],
+			RIGHT: [Vector2.ZERO, Vector2.RIGHT.times(2), Vector2.LEFT, Vector2.add(Vector2.RIGHT.times(2), Vector2.UP), Vector2.add(Vector2.LEFT, Vector2.DOWN.times(2))],
+		},
+		{
 			// angle: 270
-			[Vector2.ZERO, Vector2.RIGHT, Vector2.LEFT.times(2), new Vector2(-2, 1), Vector2.RIGHT.add(Vector2.UP.times(2))],
-			[Vector2.ZERO, Vector2.LEFT.times(2), Vector2.RIGHT, Vector2.RIGHT.add(Vector2.DOWN.times(2)), new Vector2(-2, -1)],
-		],
+			LEFT: [Vector2.ZERO, Vector2.RIGHT, Vector2.LEFT.times(2), Vector2.add(Vector2.LEFT.times(2), Vector2.DOWN), Vector2.add(Vector2.RIGHT, Vector2.UP.times(2))],
+			RIGHT: [Vector2.ZERO, Vector2.LEFT.times(2), Vector2.RIGHT, Vector2.add(Vector2.RIGHT, Vector2.DOWN.times(2)), Vector2.add(Vector2.LEFT.times(2), Vector2.UP)],
+		},
 	],
-]
+];
+
+console.log(vectorPattern[0][0].LEFT);
